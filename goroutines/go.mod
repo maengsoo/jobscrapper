6efae6aa77b99d14cb156b787c9a89d20c68ca01
장명수 <dev.maeng@gmail.com>
2021-05-27 23:25:01 +0900

    feat: go루틴 공부 후 urlchecker 리팩토링
    -연산속도 10초 -> 1초
    -기본적인 go 언어 문법공부
    -자주 쓰이는 로직 공부

---
+++ goroutines/go.mod
@@ -0,0 +1,3 @@
+module github.com/maengsoo/learngo/goroutines
+
+go 1.16
--- urlchecker/main.go
+++ urlchecker/main.go
@@ -6,10 +6,16 @@ import (
 	"net/http"
 )
 
+type requestResult struct {
+	url    string
+	status string
+}
+
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
-	var results = make(map[string]string)
+	results := make(map[string]string)
+	c := make(chan requestResult)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -23,25 +29,27 @@ func main() {
 	}
 
 	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
+		go hitURL(url, c)
 	}
 
-	for url, result := range results {
-		fmt.Println(url, result)
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
 	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+
 }
 
-func hitURL(url string) error {
+func hitURL(url string, c chan<- requestResult) {
 	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode >= 400 {
 		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
+		c <- requestResult{url: url, status: "FAILD"}
+	} else {
+		c <- requestResult{url: url, status: "OK"}
 	}
-	return nil
 }
