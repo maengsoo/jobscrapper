604c2a71be13cac5ae0275a7e71f013c69e6f15b
장명수 <dev.maeng@gmail.com>
2021-05-31 04:11:35 +0900

    feat: main 함수에 각페이지를 구조체인 배열상태로 저장하는 메소드 추가

--- jobscrapper/main.go
+++ jobscrapper/main.go
@@ -22,16 +22,21 @@ type extractedJob struct {
 var baseURL string = "https://kr.indeed.com/jobs?q=java&limit=50"
 
 func main() {
+	var jobs []extractedJob
 	totalPages := getPages()
 
 	for i := 0; i < totalPages; i++ {
-		getPage(i)
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
 	}
+	fmt.Println(jobs)
 }
 
-func getPage(page int) {
+// 한페이지 배열을 나열하는 메소드
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
-	fmt.Println("Requesting", pageURL)
+	fmt.Println("Requesting:", pageURL)
 	res, err := http.Get(pageURL)
 	checkErr(err)
 	checkCode(res)
@@ -45,13 +50,29 @@ func getPage(page int) {
 
 	// html class, id 에서 정보가져오기
 	searchCards.Each(func(i int, card *goquery.Selection) {
-		id, _ := card.Attr("data-jk")
-		title := cleanString(card.Find(".jobTitle>span").Text())
-		name := cleanString(card.Find(".companyName").Text())
-		location := card.Find(".company_location .companyLocation").Text()
-
-		fmt.Println(id, title, name, location)
+		job := extractJob(card)
+		jobs = append(jobs, job)
 	})
+
+	return jobs
+}
+
+// id, title, name, location 정보 가져오기
+func extractJob(card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("data-jk")
+	title := cleanString(card.Find(".jobTitle>span").Text())
+	name := cleanString(card.Find(".companyName").Text())
+	location := card.Find(".company_location .companyLocation").Text()
+	salary := cleanString(card.Find(".salaryText").Text())
+	summary := cleanString(card.Find(".job-snippet").Text())
+
+	return extractedJob{
+		id:       id,
+		title:    title,
+		name:     name,
+		location: location,
+		salary:   salary,
+		summary:  summary}
 }
 
 // 가져온 id, title, name, location 의 공백을 없애고(TrimSpace), 배열을 만들고(Fields), 배열을 스페이스를 기준으로 한줄에 string(Join)으로 만듬
